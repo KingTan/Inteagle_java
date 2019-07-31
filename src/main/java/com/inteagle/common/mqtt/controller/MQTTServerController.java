5083e0b47921298d6243ce3836a5e33ca98d0405
IVAn <1179339295@qq.com>
2019-07-31 19:11:03 +0800

    完成发送时间同步数据

--- src/main/java/com/inteagle/common/mqtt/controller/MQTTServerController.java
+++ src/main/java/com/inteagle/common/mqtt/controller/MQTTServerController.java
@@ -1,5 +1,10 @@
 package com.inteagle.common.mqtt.controller;
 
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,32 +30,13 @@ public class MQTTServerController {
 	}
 
 	@RequestMapping("/send/msg")
-	public boolean send(@RequestParam("msg") String msg, @RequestParam("topic") String topic) throws MqttException {
+	public boolean send(@RequestParam("msg") String msg, @RequestParam("topic") String topic)
+			throws MqttException, StructException {
 
 //		System.out.println("发布消息");
 //		System.out.println(Thread.currentThread().getStackTrace()[1].getMethodName() + "==========:" + msg);
 
 		// a5a5 0008 2006 000003e8000007d0 11 5a5a
-		String sof = "a5a5";
-		sof = ByteHexUtil.changeType(sof);
-
-		String len = "0008";
-		len = ByteHexUtil.changeType(len);
-
-		String cmd = "2006";
-		cmd = ByteHexUtil.changeType(cmd);
-
-		String data = "000003e8000007d0";
-		data = ByteHexUtil.changeType(data);
-		
-		String crc = "11";
-		crc = ByteHexUtil.changeType(crc);
-
-		String eof = "5a5a";
-		eof = ByteHexUtil.changeType(eof);
-
-		msg = sof + len + cmd + data + crc + eof;
-
 //		String sof = "a5a5";
 //		sof = ByteHexUtil.changeType(sof);
 //
@@ -62,31 +48,97 @@ public class MQTTServerController {
 //
 //		String data = "000003e8000007d0";
 //		data = ByteHexUtil.changeType(data);
-//
-//		String crc = "";
-//		crc = len + cmd + data;
-//
-//		byte[] crc_byte = ByteHexUtil.hex2Byte(crc);
-//
-//		byte crc_after = CRC8.calcCrc8(crc_byte);
-//		System.out.println("crc_after------" + crc_after);
-//
-//		crc = ByteHexUtil.changeType(Byte.toString(crc_after));
-//
-//		// CRC 10进制
-//		long crc_ten = Long.parseLong(crc, 16);
-//		System.out.println("crc_ten------" + crc_ten);
 //		
-////		crc = Byte.toString(crc_after);
-//		crc = Long.toString(crc_ten);
-//		System.out.println("crc------" + crc);
+//		String crc = "11";
+//		crc = ByteHexUtil.changeType(crc);
 //
 //		String eof = "5a5a";
 //		eof = ByteHexUtil.changeType(eof);
 //
 //		msg = sof + len + cmd + data + crc + eof;
-//		System.out.println("msg-----" + msg);
+
+		String sof = "a5a5";
+		sof = ByteHexUtil.changeType(sof);
+
+		String len = "0008";
+		len = ByteHexUtil.changeType(len);
+
+		String cmd = "2006";
+		cmd = ByteHexUtil.changeType(cmd);
+
+		// 初始时间结构体对象
+		TimeSyncData timeSyncData = new TimeSyncData();
+
+		long curretnTimeStamp = Calendar.getInstance().getTimeInMillis();
+		
+		System.out.println("curretnTimeStamp-------" + curretnTimeStamp);
+		
+		int t_l = (int) curretnTimeStamp;
+		int t_h = (int) (curretnTimeStamp >> 32);
+		
+		System.out.println("t_h-------" + t_h);
+		System.out.println("t_l-------" + t_l);
+		
+		Long change_val = getLongFromInt(t_l, t_h);
+		System.out.println("change_val-------" + change_val);
+
+//		int t_h = Integer.parseInt(curretnTimeStamp.toString().substring(0, 7));
+//		int t_l = Integer.parseInt(curretnTimeStamp.toString().substring(7, curretnTimeStamp.toString().length()));
+
+		timeSyncData.setT_h(t_h);
+		timeSyncData.setT_l(t_l);
+
+		// 转成字节数组
+		byte[] time_byte = JavaStruct.pack(timeSyncData);
+		String data = ByteHexUtil.bytes2HexStr(time_byte);
+		// System.out.println("data------"+data);
+
+		// 按照协议 截取到crc的16进制值
+		String crc = len + cmd + data;
+		// System.out.println("crc-------" + crc);
+
+		// 16进制转成字节数组
+		byte[] crc_byte = ByteHexUtil.hex2Byte(crc);
+
+		// 传入字节数组 求出crc的校验值字节
+		byte crc_after = CRC8.calcCrc8(crc_byte);
+		// System.out.println("crc_after-校验值-----" + crc_after);
+
+		// 将校验值字节放入数组中 转成16进制数据
+		byte[] crc_after_array = { crc_after };
+		crc = ByteHexUtil.byte2HexStr(crc_after_array);
+		// System.out.println("crc_last------" + crc);
+
+		String eof = "5a5a";
+		eof = ByteHexUtil.changeType(eof);
+
+		msg = sof + len + cmd + data + crc + eof;
+		System.out.println("msg-----" + msg);
 
 		return iEmqService.publish(topic, msg);
+	}
+
+	// long转为两个int
+	public static List<Integer> changLong(long num) {
+		int intNum1 = (int) num;
+		int intNum2 = (int) (num >> 32);
+		List<Integer> list = new ArrayList<Integer>();
+		list.add(intNum1);
+		list.add(intNum2);
+		System.out.println("intNum1-----" + intNum1);
+		System.out.println("intNum2-----" + intNum2);
+		return list;
+	}
+
+	// 两个int合并成long
+	public static long getLongFromInt(int num1, int num2) {
+		// long l1 = (long)(((long)num2&0xffffffff)<<32);
+		// long l2 = (long)num1&0x00000000ffffffffL;
+		// 简写
+		long l1 = (num2 & 0x00000000ffffffffL) << 32;
+		long l2 = num1 & 0x00000000ffffffffL;
+
+		long num = l1 | l2;
+		return num;
 	}
 }
